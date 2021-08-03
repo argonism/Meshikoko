111464d0f8e25618b7ef7a79c0f89a127a83c82e
k-usuha <kouta528bado@yahoo.co.jp>
2021-08-04 01:13:21 +0900

    update: for gce

--- api/src/main.go
+++ api/src/main.go
@@ -6,7 +6,7 @@ import (
     "app/config"
     "app/database"
     "app/server"
-    // "app/models"
+    "app/models"
 )
 
 func main() {
@@ -15,51 +15,9 @@ func main() {
     flag.Parse()
 
     config.Init(*env)
-    // database.Init(true, models.GetModels()...)
+    database.Init(true, models.GetModels()...)
     defer database.Close()
     if err := server.Init(); err != nil {
         panic(err)
     }
 }
-
-
-// [START import]
-// import (
-// 	"fmt"
-// 	"log"
-// 	"net/http"
-// 	"os"
-// )
-
-// // [END import]
-// // [START main_func]
-
-// func main() {
-// 	http.HandleFunc("/", indexHandler)
-
-// 	// [START setting_port]
-// 	port := os.Getenv("PORT")
-// 	if port == "" {
-// 		port = "8080"
-// 		log.Printf("Defaulting to port %s", port)
-// 	}
-
-// 	log.Printf("Listening on port %s", port)
-// 	if err := http.ListenAndServe(":"+port, nil); err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	// [END setting_port]
-// }
-
-// // [END main_func]
-
-// // [START indexHandler]
-
-// // indexHandler responds to requests with our greeting.
-// func indexHandler(w http.ResponseWriter, r *http.Request) {
-// 	if r.URL.Path != "/" {
-// 		http.NotFound(w, r)
-// 		return
-// 	}
-// 	fmt.Fprint(w, "Hello, World!")
-// }
